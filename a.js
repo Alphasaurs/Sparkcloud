aa49ee13c156f151d467b5efd502b3b8bc9d38b4
Abhishek <abhishekalacrity@gmail.com>
2020-08-06 14:38:28 +0530

    Delete a.js

--- a.js
+++
@@ -1,9 +0,0 @@
-fetch("a.wasm")
-    .then(response => response.arrayBuffer())
-    .then(bytes => WebAssembly.instantiate(bytes))
-    .then(response => {
-        let result = response.instance.exports.add(1,6);
-        console.log(result);
-    });
-    
-
